2c926554e73c975a251dc5f614a6deed74a18141
Thoka88 <cortesthoka88@gmail.com>
2025-08-06 11:04:27 -0600

    Conectar hyperlinks
    Se conectaron los hyperlinks para poder ingresar a otras vistas

--- Proyecto_Museo_Dragones/src/main/java/com/uisil/proyecto_museo_dragones/controllers/LoginController.java
+++ Proyecto_Museo_Dragones/src/main/java/com/uisil/proyecto_museo_dragones/controllers/LoginController.java
@@ -2,9 +2,16 @@ package com.uisil.proyecto_museo_dragones.controllers;
 
 import com.uisil.proyecto_museo_dragones.App;
 import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class LoginController {
 
@@ -13,6 +20,12 @@ public class LoginController {
 
     @FXML
     private PasswordField txtContrasena;
+    
+    @FXML 
+    private Hyperlink linkContrasena;
+    
+    @FXML 
+    private Hyperlink linkRegistro;
 
     @FXML
     private void IniciarSesion() {
@@ -23,4 +36,32 @@ public class LoginController {
         System.out.println("Usuario: " + usuario);
         System.out.println("Contraseña: " + contrasena);
     }
+    @FXML
+    private void RecuperarContrasena(ActionEvent event) {
+    System.out.println("Redirigiendo a la vista de login...");
+    // Aquí puedes cargar otra escena, por ejemplo:
+    try {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/uisil/proyecto_museo_dragones/views/RecuperarContrasena.fxml"));
+        Parent root = loader.load();
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
 }
+     @FXML
+    private void RegistrarCuenta(ActionEvent event) {
+    System.out.println("Redirigiendo a la vista de login...");
+    // Aquí puedes cargar otra escena, por ejemplo:
+    try {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/uisil/proyecto_museo_dragones/views/Registro.fxml"));
+        Parent root = loader.load();
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+}
+}    
